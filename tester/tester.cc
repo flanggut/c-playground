8c8fbbb724113b5aa5253206b9dc9b675266d5db
Fabian Langguth <fabian.langguth@gmail.com>
2015-08-25 15:45:22 +0200

    tester with std::stod

--- tester/tester.cc
+++ tester/tester.cc
@@ -4,9 +4,11 @@
 int main(int argc, char **argv)
 {
     std::stringstream ss("10.2345asdf");
-    double ret;
-    ss >> ret;
+    double ret1;
+    ss >> ret1;
+    double ret2 = std::stod("10.2345asdf");
 
-    std::cout << "Double: " << ret << std::endl;
+    std::cout << "std::stringstream Double: " << ret1 << std::endl;
+    std::cout << "std::stod         Double: " << ret2 << std::endl;
     return 0;
 }
